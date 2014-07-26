bfdde6249405ba575a6fc04d9aefb9887c0c4f41
吉亨斗 <kilh@yoshitooruto-no-MacBook-Air.local>
2014-07-26 23:19:09 +0900

    ios8 beta4 対応、aws s3からテスト用xml取得

--- swift-news-application/ViewController.swift
+++ swift-news-application/ViewController.swift
@@ -11,16 +11,16 @@ import Foundation
 
 class ViewController: UIViewController, NSXMLParserDelegate, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet var blog_area: UITableView
+    @IBOutlet var blog_area: UITableView!
     
-    var rssList = String[]()
-    var element:String = ""
+    public var rssList = [String]()
+    public var element:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // rssデータを取得する処理
-        let url : NSURL = NSURL(string: "http://rss.dailynews.yahoo.co.jp/fc/entertainment/rss.xml")
+        let url : NSURL = NSURL(string: "https://s3-ap-northeast-1.amazonaws.com/kilobject/talent-news.xml")
         
         // rssデータ取得
         let myparser:NSXMLParser = NSXMLParser(contentsOfURL:url)
@@ -38,7 +38,7 @@ class ViewController: UIViewController, NSXMLParserDelegate, UITableViewDelegate
     
     func tableView(tableView: UITableView?, cellForRowAtIndexPath indexPath:NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
-        cell.text = rssList[indexPath.row]
+        cell.textLabel.text = rssList[indexPath.row]
         return cell;
     }
     
@@ -61,6 +61,11 @@ class ViewController: UIViewController, NSXMLParserDelegate, UITableViewDelegate
         if element == "title" && !string.isEmpty {
             rssList.append(string)
         }
+        
+        // elementがblog_imageの場合
+        // imageバスを取得してNSDataでデータ取得
+        // UIImageでイメージ変換
+        // UIImageViewを生成し、cell.backgroundView = imageView;でcellの背景を設定する
     }
     
     // 要素の終了時
